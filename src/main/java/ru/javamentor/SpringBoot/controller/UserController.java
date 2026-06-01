25241a6d80f465fd7a28e73930fbd27ce2b3256a
Developavel <pa.mordovin@gmail.com>
2026-06-01 11:28:36 +0300

    Update HelloController.java and UserController.java

--- src/main/java/ru/javamentor/SpringBoot/controller/UserController.java
+++ src/main/java/ru/javamentor/SpringBoot/controller/UserController.java
@@ -1,19 +1,20 @@
 package ru.javamentor.SpringBoot.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.javamentor.SpringBoot.model.User;
 import ru.javamentor.SpringBoot.service.UserService;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
-
     private final UserService userService;
 
-    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
