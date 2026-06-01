6cec990b1da4a07789bee8daf95718fbf1a1d295
Developavel <pa.mordovin@gmail.com>
2026-06-01 11:30:51 +0300

    Update UserService.java.java and UserServiceImpl.java.java

--- src/main/java/ru/javamentor/SpringBoot/service/UserServiceImpl.java
+++ src/main/java/ru/javamentor/SpringBoot/service/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package ru.javamentor.SpringBoot.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javamentor.SpringBoot.dao.UserDao;
@@ -13,7 +12,6 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     private final UserDao userDao;
 
-    @Autowired
     public UserServiceImpl(UserDao userDao) {
         this.userDao = userDao;
     }
