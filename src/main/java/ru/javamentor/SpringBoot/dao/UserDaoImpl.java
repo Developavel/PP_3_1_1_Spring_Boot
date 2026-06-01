a8dcbe8da106cf792152ce7c3646b8586151952a
Developavel <pa.mordovin@gmail.com>
2026-06-01 11:29:24 +0300

    Update UserDao.java and UserDaoImpl.java

--- src/main/java/ru/javamentor/SpringBoot/dao/UserDaoImpl.java
+++ src/main/java/ru/javamentor/SpringBoot/dao/UserDaoImpl.java
@@ -1,17 +1,18 @@
 package ru.javamentor.SpringBoot.dao;
 
+import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 import ru.javamentor.SpringBoot.model.User;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
 public class UserDaoImpl implements UserDao {
+    private final EntityManager entityManager;
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    public UserDaoImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public List<User> listUsers() {
